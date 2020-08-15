86fe964a14a78297d03f3a91e8ba29af761a47d7
Jeff King <jazfunk@gmail.com>
2020-08-14 20:40:59 -0400

    Further implementation of the client react app

--- API/client/src/App.js
+++ API/client/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "./Components/Header";
 
 function App() {
--- Core/User.cs
+++ Core/User.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+
+using System.ComponentModel.DataAnnotations;
 
 namespace Core
 {
     public class User
     {
+        [Key]
         public virtual int Id { get; set; }
+
+        [Required]
         public virtual string FirstName { get; set; }
+
+        [Required]
         public virtual string LastName { get; set; }
+
+        [Required]
         public virtual string PassWord { get; set; }
+
+        [Required]
         public virtual string Email { get; set; }
     }
 }
